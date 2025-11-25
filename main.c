bd6f2656c08ed2170b5621eb1620a57a66e948f4
Your Name <brian95liao@gmail.com>
2025-11-25 16:02:25 +0800

    finish int_sub

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 int add(int a,int b){
     return a+b;
 }
+
+int sub(int a,int b){
+    return a-b;
+}
+
 int main(){
     int a=5,b=13;
     printf("%d + %d = %d\n",a,b,add(a,b));
+    printf("%d - %d = %d\n",a,b,sub(a,b));
+
     return 0;
 }
